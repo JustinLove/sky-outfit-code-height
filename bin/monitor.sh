0c824eab14e9782266ff97e2b3cd04fbefa24e18
Justin Love <git@JustinLove.name>
2025-11-08 09:59:58 -0600

    hey no debug, we can just optimize

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-watch "elm make src/SkyOutfitCodeHeight.elm --output public/sky-outfit-code-height.js" src
+watch "elm make src/SkyOutfitCodeHeight.elm --optimize --output public/sky-outfit-code-height.js" src
